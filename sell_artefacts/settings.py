fe39d5dc0cba6cf3f5b8aa77bf3df175171303b1
keefm6776 <keefm_6776@yahoo.com>
2019-09-22 11:14:37 +0000

    Include Stripe View

--- sell_artefacts/settings.py
+++ sell_artefacts/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#g#2j!w4=dlqyyy4cn7pno#47q!i-o3^6obw^fkcjut26w^oqk'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
