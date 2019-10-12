49711c9d14648e6348782ac1ec40727048a93127
keefm6776 <keefm_6776@yahoo.com>
2019-10-12 19:22:28 +0000

    Rename bids without using _

--- bids/models.py
+++ bids/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Current_Bid(models.Model):
+class AllBids(models.Model):
     artefact_name = models.CharField(max_length=50, default='')
     user_email =  models.CharField(max_length=50, default='')
     bid = models.DecimalField(max_digits=9, decimal_places=2)
